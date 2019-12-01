66e332105fc7fd6636b469a4da61c0ed0e1bb9ea
Zachary Chandler <soap.radio21@gmail.com>
2019-12-01 08:55:57 -0600

    add terminal interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import canvas from './canvas.js'
 
 console.log(new canvas())
+
+$('#terminal').terminal(
+  function(command) {
+    if (command == 'test') {
+      this.echo("you just typed 'test'")
+    } else {
+      this.echo('unknown command')
+    }
+  },
+  { prompt: '>', name: 'test' }
+)
