503ba2faad5f77e21d2d48ce2da56b7760a65179
Zachary Chandler <soap.radio21@gmail.com>
2019-12-02 03:12:34 -0800

    Add coords command

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import canvas from './canvas.js'
 
-console.log(new canvas())
+let c = new canvas()
+
+let commands = {
+  coords: self =>
+    self.echo(`${Math.round(c.xOffset)}, ${Math.round(c.yOffset)}`)
+}
 
 $('#terminal').terminal(
   function(command) {
-    if (command == 'test') {
-      this.echo("you just typed 'test'")
-    } else {
-      this.echo('unknown command')
+    if (command) {
+      let args = command.split(/\s+/)
+      let exe = commands[args[0]]
+
+      if (exe) {
+        exe(this, args)
+      } else {
+        this.echo('unknown command')
+      }
     }
   },
   { prompt: '>', name: 'test' }
